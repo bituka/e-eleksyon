c5b81ff7cd6c74cb1c213e66b45774bf82c8b9af
MangKong-coder <domango1214@gmail.com>
2021-06-28 00:33:28 +0800

    upload-main-ui

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div id="main">
+  <Pic />
+  <form action="">
+    <Form />
+  </form>
+  
+  
+</div>
+  
+</template>
+
+<script>
+import Pic from "./components/Pic.vue";
+import Form from "./components/Form.vue";
+
+export default {
+  name: 'Main',
+  components: {
+    Pic,
+    Form,
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/Pic.vue
@@ -0,0 +1,17 @@
+<template>
+  <img class="pic" src="../assets/Portrait_Placeholder.png" alt="">
+</template>
+
+<script>
+export default {
+    name: 'Pic'
+}
+</script>
+
+<style scoped>
+.pic {
+    float:right;
+    width: 7%;
+    margin-right: 10%;
+}
+</style>

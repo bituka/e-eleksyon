ecd494df83ecdfdd134c69213841051138ea32a3
Garry G <goryo.webdev@gmail.com>
2021-07-19 08:36:54 +0800

    merging latest frontend changes

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="container">
+    <Navbar />
+    <div class="row">
+        <Position />
+    <Voters />
+    </div>
+    
+    <Candidates />
+    
+</div>
+  
+</template>
+
+<script>
+import Position from "../components/Positions.vue";
+import Candidates from "../components/Candidates.vue";
+import Voters from "../components/Voters.vue";
+import Navbar from "../components/Navbar.vue";
+
+
+
+export default {
+    name: 'Dashboard',
+    components: {
+        Position,
+        Candidates,
+        Voters,
+        Navbar
+    } 
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
